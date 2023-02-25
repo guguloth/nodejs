704ed948c8154e3cc4db516ff76d05cd641e6ec1
Gopal naik guguloth <gugulothgopalnaik@gmail.com>
2023-02-25 21:17:33 +0530

    ssl certi with npm mkcert

--- expressGenerator/routes/users.js
+++ expressGenerator/routes/users.js
@@ -54,7 +54,7 @@ router.get('/', cors.corsOptions, authenticate.verifyUser, authenticate.adminUse
   res.json({success: true, token: token, status: 'Your are successfully loggedin!'});
 })
 
-router.get('facebook/token',passport.authenticate('facebook-token'),(req, res) => {
+router.get('/facebook/token',passport.authenticate('facebook-token',{session: false}),(req, res) => {
   var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type','application/json');
